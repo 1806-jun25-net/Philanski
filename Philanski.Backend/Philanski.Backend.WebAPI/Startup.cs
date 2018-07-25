81501bb65b1efabebe884c2f765d3d5a4fdb6150
Daniel Ramskill <dramskil@uncc.edu>
2018-07-25 14:27:21 -0400

    added functionality for Swagger UI

--- Philanski.Backend/Philanski.Backend.WebAPI/Startup.cs
+++ Philanski.Backend/Philanski.Backend.WebAPI/Startup.cs
@@ -77,6 +77,7 @@ namespace Philanski.Backend.WebAPI
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
 
             app.UseHttpsRedirection();
