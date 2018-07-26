15535948e1cab3b32c1a9c3f42a936b14c34524d
Daniel Ramskill <dramskil@uncc.edu>
2018-07-26 13:41:51 -0400

    removed empty space above navbar

---
+++ Philanski.Frontend/Philanski.Frontend.MVC/Controllers/TimeSheetController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Philanski.Frontend.MVC.Models;
+
+namespace Philanski.Frontend.MVC.Controllers
+{
+    public class TimeSheetController : Controller
+    {
+        public readonly static string ServiceUri = "https://localhost:44386/api/";
+
+        public HttpClient HttpClient { get; }
+        // GET: Department
+
+        public TimeSheetController(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var uri = ServiceUri + "timesheet";
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            try
+            {
+                var response = await HttpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+
+                List<TimeSheets> timeSheet = JsonConvert.DeserializeObject<List<TimeSheets>>(jsonString);
+
+                return View(timeSheet);
+            }
+            catch (HttpRequestException ex)
+            {
+                // logging
+                return View("Error");
+            }
+        }
+    }
+}
