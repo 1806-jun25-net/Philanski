0afda7b3eea6390c87e81828956f6f26b8ea5fe6
Phil <philipaplatt@gmail.com>
2018-07-27 13:44:34 -0400

    added identity controllers to mvc

---
+++ Philanski.Frontend/Philanski.Frontend.MVC/Controllers/AccountController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Philanski.Frontend.MVC.Models;
+
+namespace Philanski.Frontend.MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : AServiceController
+    {
+
+        public AccountController(HttpClient httpClient) : base(httpClient)
+        { }
+
+        // GET: Account/Register
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public async Task<ActionResult> Register(Login account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Error");
+            }
+
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Post, "api/Account/Login", account);
+
+            HttpResponseMessage apiResponse;
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+            PassCookiesToClient(apiResponse);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Account/Login
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        public async Task<ActionResult> Login(Login account)
+        {
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Post, "api/Account/Login", account);
+
+            HttpResponseMessage apiResponse;
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch (AggregateException ex)
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                if (apiResponse.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    return View("AccessDenied");
+                }
+                return View("Error");
+            }
+
+            PassCookiesToClient(apiResponse);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Account/Logout
+        public async Task<ActionResult> Logout()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Error");
+            }
+
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Post, "api/Account/Logout");
+
+            HttpResponseMessage apiResponse;
+
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch (AggregateException ex)
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+            PassCookiesToClient(apiResponse);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool PassCookiesToClient(HttpResponseMessage apiResponse)
+        {
+            if (apiResponse.Headers.TryGetValues("Set-Cookie", out IEnumerable<string> values))
+            {
+                string authValue = values.FirstOrDefault(x => x.StartsWith(s_CookieName));
+                if (authValue != null)
+                {
+                    Response.Headers.Add("Set-Cookie", authValue);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
