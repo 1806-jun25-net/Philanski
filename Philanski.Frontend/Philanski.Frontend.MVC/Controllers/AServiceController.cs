82e956a77d497637eace8d4145a073010e422a9e
diane vonah <lance.revature@outlook.com>
2018-07-30 11:55:01 -0400

    made cookies unsecure. THEY WORK

--- Philanski.Frontend/Philanski.Frontend.MVC/Controllers/AServiceController.cs
+++ Philanski.Frontend/Philanski.Frontend.MVC/Controllers/AServiceController.cs
@@ -14,13 +14,9 @@ namespace Philanski.Frontend.MVC.Controllers
     public class AServiceController : Controller
     {
 
-<<<<<<< HEAD
-    //    private static readonly Uri s_serviceUri = new Uri("https://localhost:44386/");
-=======
-
->>>>>>> 3fdcc76848cbd5a6792e0edf862814163c499bf4
-        private static readonly Uri s_serviceUri = new Uri("https://philanksi.azurewebsites.net/");
-        protected static readonly string s_CookieName = "PhilanskiApiAuth";
+       private static readonly Uri s_serviceUri = new Uri("https://localhost:44386/");
+    //    private static readonly Uri s_serviceUri = new Uri("https://philanksi.azurewebsites.net/");
+        protected static readonly string s_CookieName = "PhilanskiApiAuth2";
 
         protected HttpClient HttpClient { get; }
 
