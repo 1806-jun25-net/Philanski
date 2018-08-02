98f09ce85f23d2a1d07ff634ab6633645d18c534
Phil <philipaplatt@gmail.com>
2018-08-01 20:48:41 -0400

    preliminary test for repomethods

--- Philanski.Backend/Philanski.Backend.Testing/UnitTest1.cs
+++ Philanski.Backend/Philanski.Backend.Testing/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Moq;
 using Philanski.Backend.DataContext.Models;
 using Philanski.Backend.Library.Models;
@@ -8,13 +11,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace Philanski.Backend.Testing
 {
     public class UnitTest1
     {
 
-        PhilanskiManagementSolutionsContext db = new PhilanskiManagementSolutionsContext();
+
+
+        //ensuredatabasecreated still needs to be used somewhere. 
+        //  DbContextOptionsBuilder<PhilanskiManagementSolutionsContext> optionsBuilder = new DbContextOptionsBuilder<PhilanskiManagementSolutionsContext>().UseInMemoryDatabase();
+        //public static DbContextOptionsBuilder UseInMemoryDatabase(this DbContextOptionsBuilder optionsBuilder, Action<InMemoryDbContextOptionsBuilder> inMemoryOptionsAction = null);
+    //    optionsBuilder
+    //.UseSqlServer(connectionString, providerOptions=>providerOptions.CommandTimeout(60))
+    //.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
+      //  PhilanskiManagementSolutionsContext db = new PhilanskiManagementSolutionsContext();
+
+        
+        
 
 
         [Theory]
@@ -26,7 +42,7 @@ namespace Philanski.Backend.Testing
             var testDate = Convert.ToDateTime(test);
             var result = TimeSheetApproval.GetPreviousSundayOfWeek(testDate);
             Assert.Equal(Convert.ToDateTime(actual), result);
-
+           
         }
 
         [Theory]
@@ -84,26 +100,27 @@ namespace Philanski.Backend.Testing
         [Fact]
         public void GetEmployeeByIDRepoMethodShouldWork()
         {
-            // arrange
-            Employee employeebyID = new Employee();
-            employeebyID.Id = 1;
-            employeebyID.FirstName = "Phil";
-            employeebyID.LastName = "Platt";
-            employeebyID.Email = "bobman56@gmail.com";
-            employeebyID.JobTitle = "Pancake Flipper";
-            employeebyID.WorksiteId = 3;
-            employeebyID.Salary = 45000.00m;
-            employeebyID.HireDate = DateTime.Now;
-           
-                
 
-            var mockRepo = new Mock<Repository>();
-           // mockRepo.Setup(x => x.GetEmployeeByID(1)).Returns(employeebyID);
-           //Get Employee By ID is now async
+            var options = new DbContextOptionsBuilder<PhilanskiManagementSolutionsContext>()
+                 .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                 .Options;
 
-           
-               
-            
+
+            // Run the test against one instance of the context
+            using (var context = new PhilanskiManagementSolutionsContext(options))
+            {
+                var repo = new Repository(context);
+               Task<Employee> emp = repo.GetEmployeeByID(1);
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            //using (var context = new BloggingContext(options))
+            {
+                //Assert.AreEqual(1, context.Blogs.Count());
+                //Assert.AreEqual("http://sample.com", context.Blogs.Single().Url);
+            }
+
+
 
         }
 
