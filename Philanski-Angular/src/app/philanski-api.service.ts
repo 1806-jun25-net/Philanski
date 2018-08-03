ef4e9600c13f6cb2fe61a610c4e03a41d609c694
diane vonah <lance.revature@outlook.com>
2018-08-03 10:47:08 -0400

    updating for vsts demo

--- Philanski-Angular/src/app/philanski-api.service.ts
+++ Philanski-Angular/src/app/philanski-api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Login } from './models/Login';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class PhilanskiApiService {
   //private readonly Url : string = 'https://philanksi.azurewebsites.net/api/'
   constructor(private httpClient: HttpClient) { }
 
-  postLogin(login: Login){
+  /*postLogin(login: Login){
     var body = JSON.stringify(login)
     var header = new HttpHeaders({ 
     'Access-Control-Allow-Origin': '*',
@@ -21,7 +23,25 @@ export class PhilanskiApiService {
     'Allow' : 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
   })
     return this.httpClient.post(this.Url + 'Account/Login', body, {headers: header, withCredentials: true} )
-  }
+  }*/
+  postLogin(login: Login) {
+    var body = JSON.stringify(login)
+    var header = new HttpHeaders({ 
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Origin , Access-Control-* , X-Requested-With, Accept',
+    'Content-Type':  'application/json,charset=utf-8',
+    'Accept': 'application/json',
+    'Allow' : 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+  });
+    return this.httpClient.post(this.Url + 'Account/Login', body, {headers: header, withCredentials: true})
+           .pipe(map(data : any) => {
+            data.json();
+            // the console.log(...) line prevents your code from working 
+            // either remove it or add the line below (return ...)
+            console.log("I CAN SEE DATA HERE: ", data.json());
+            return data.json();
+    });
+}
   postLogout()
   {
     var header = new HttpHeaders({ 
