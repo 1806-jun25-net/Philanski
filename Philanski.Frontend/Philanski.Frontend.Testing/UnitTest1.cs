84c2f499866da38285ed150a1e0707db0725bd0b
Phil <philipaplatt@gmail.com>
2018-08-04 21:45:29 -0400

    tested department and employee controller

--- Philanski.Frontend/Philanski.Frontend.Testing/UnitTest1.cs
+++ Philanski.Frontend/Philanski.Frontend.Testing/UnitTest1.cs
@@ -1,14 +1,80 @@
+using Philanski.Frontend.Library.Models;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using Xunit;
+using Moq;
+using Moq.Protected;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using Philanski.Frontend.MVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Philanski.Frontend.Testing
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestingDepartmentController()
         {
+      
+
+                // Arrange 2
+                //const string testContent = "test content";
+                var mockMessageHandler = new Mock<HttpMessageHandler>();
+
+
+            //below was from example online but I couldn't make much sense of it. consider deleting. test passes without it. 
+                //mockMessageHandler.Protected()
+                //    .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                //    .Returns(Task.FromResult(new HttpResponseMessage
+                //    {
+                //        StatusCode = HttpStatusCode.OK,
+                //        Content = new StringContent(testContent)
+                //    }));
+
+
+            HttpClient client = new HttpClient(mockMessageHandler.Object);
+
+            DepartmentController controller = new DepartmentController(client);
+
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+           Assert.IsAssignableFrom<Task<ActionResult>>(result);
+            Assert.Equal(1, 1);
+
+
 
         }
+
+        [Fact]
+        public void TestingEmployeeController()
+        {
+           
+            // Arrange 2
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+          
+
+            HttpClient client = new HttpClient(mockMessageHandler.Object);
+
+            EmployeeController controller = new EmployeeController(client);
+
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsAssignableFrom<Task<ActionResult>>(result);
+            Assert.Equal(1, 1);
+
+
+
+        }
+
+
     }
 }
