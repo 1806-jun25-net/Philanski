cd0fed7b90505cb64c3e23b082ceb08abc02055e
diane vonah <lance.revature@outlook.com>
2018-08-04 20:19:16 -0400

    changed http header

--- Philanski-Angular/src/app/philanski-api.service.ts
+++ Philanski-Angular/src/app/philanski-api.service.ts
@@ -11,8 +11,8 @@ export class PhilanskiApiService {
   private readonly Url : string = 'https://localhost:44386/api/'
   //private readonly Url : string = 'https://philanksi.azurewebsites.net/api/'
   private readonly header = new HttpHeaders({ 
-    'Access-Control-Allow-Origin': 'true',
-    'Access-Control-Allow-Credentials':'true',
+  //  'Access-Control-Allow-Origin': 'true',
+   // 'Access-Control-Allow-Credentials':'true',
    // 'Access-Control-Allow-Origin': '*',
    // 'Access-Control-Allow-Headers': 'Content-Type, Origin , Access-Control-* , X-Requested-With, Accept',
     'Content-Type':  'application/json,charset=utf-8',
