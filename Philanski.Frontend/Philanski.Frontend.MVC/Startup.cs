5856c20ed78f76db7deb357b8ef4c07447a52846
diane vonah <lance.revature@outlook.com>
2018-08-05 23:07:08 -0400

    removed cookie policy

--- Philanski.Frontend/Philanski.Frontend.MVC/Startup.cs
+++ Philanski.Frontend/Philanski.Frontend.MVC/Startup.cs
@@ -25,12 +25,12 @@ namespace Philanski.Frontend.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+           /* services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            });*/
 
             services.AddSingleton(sp =>
                new HttpClient(new HttpClientHandler { UseCookies = false }));
